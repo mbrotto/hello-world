6cd19d5f1736c90928ce98791208a84ed26ab140
Brett Kelley <brettkelley@geotab.com>
2020-06-22 20:58:44 +0200

    added more stuff

---
+++ helloworld.js
@@ -0,0 +1,6 @@
+let btnDisplayOutput = document.getElementById('btnDisplayOutput');
+let outputDiv = document.getElementById('output');
+
+btnDisplayOutput.addEventListener('click', function(){
+    outputDiv.innerHTML = 'Heres hoping this works';
+});
