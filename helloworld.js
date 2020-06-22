eeb1a2f06203af1414cc08a82eb20275acd306a0
Brett Kelley <brettkelley@geotab.com>
2020-06-22 21:25:10 +0200

    added more

--- helloworld.js
+++ helloworld.js
@@ -1,6 +1,33 @@
-let btnDisplayOutput = document.getElementById('btnDisplayOutput');
-let outputDiv = document.getElementById('output');
+geotab.addin.helloworld = () => {
+    // Initialize required Add-In variables
+    // Example:
+    // var element = document.getElementById("myButton");
+    // if (element) {
+    //     element.addEventListener("click", function (event) {
+    //         event.preventDefault();
+    //         alert("Action performed");
+    //     });
+    // }
 
-btnDisplayOutput.addEventListener('click', function(){
-    outputDiv.innerHTML = 'Heres hoping this works';
-});
+    let btnDisplayOutput = document.getElementById('btnDisplayOutput');
+    let outputDiv = document.getElementById('output');
+
+    btnDisplayOutput.addEventListener('click', function(){
+        outputDiv.innerHTML = 'Heres hoping this works';
+    });
+    
+    return {
+     initialize(api, state, callback) {
+
+
+
+      callback();
+     },
+     focus(api, state) {
+      // User interface is available
+     },
+     blur(api, state) {
+      // Save any Add-In state
+     }
+    };
+   };
