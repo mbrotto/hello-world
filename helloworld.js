52d0b1b18857906eda31e44cbcfc03607065a095
Brett Kelley <brettkelley@geotab.com>
2020-06-22 21:53:39 +0200

    added more

--- helloworld.js
+++ helloworld.js
@@ -9,13 +9,13 @@ geotab.addin.helloworld = () => {
     //     });
     // }
 
-    let btnDisplayOutput = document.getElementById('btnDisplayOutput');
-    let outputDiv = document.getElementById('output');
+    // let btnDisplayOutput = document.getElementById('btnDisplayOutput');
+    // let outputDiv = document.getElementById('output');
 
-    btnDisplayOutput.addEventListener('click', function(){
-        outputDiv.innerHTML = 'Heres hoping this works';
-    });
-    
+    // btnDisplayOutput.addEventListener('click', function(){
+    //     outputDiv.innerHTML = 'Heres hoping this works';
+    // });
+
     return {
      initialize(api, state, callback) {
 
